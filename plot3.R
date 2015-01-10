f155cd2286bd14cfa4d646b5258b67be9e14e6ae
GonzBio <gonzgab30@gmail.com>
2015-01-10 13:34:02 -0500

    changed plot3.R and plot3.png

--- plot3.R
+++ plot3.R
@@ -11,7 +11,8 @@ data2 <- transform(data2, Global_active_power = as.numeric(Global_active_power))
 #new column with values in column transformed into POSIXct format
 data2$DateTime <- as.POSIXct(paste(data2$Date, data2$Time), format="%d/%m/%Y %H:%M:%S") 
 #creates plot type "l" for lines
+par(pin = c(4,4))
 plot(data2$DateTime, data2$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
 lines(data2$DateTime, data2$Sub_metering_2, type = "l", col = "red")
 lines(data2$DateTime, data2$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1),col=c("black","blue","red"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.75, lty=c(1,1,1),col=c("black","blue","red"))
